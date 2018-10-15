bcc8014618f07402040cfe2b768a35c348114198
Jeremy Oliver <joliver427@icloud.com>
2018-10-15 17:24:49 -0600

    Basic layout for events page

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import Toolbar from './Navigation/Toolbar/Toolbar';
 import classes from './Events.css';
 
 class Events extends Component {
     render() {
         return(
-          <div>Hello World</div>
+          <div className={classes.background}>
+            <Toolbar />
+            <div className={classes.header}>
+              <h1>Events</h1>
+              <br></br>
+            </div>
+          </div>
+
         )
       }
     }
